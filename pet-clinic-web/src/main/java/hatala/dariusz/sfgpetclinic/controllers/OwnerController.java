254a2faafc5c0e31bedfbeed22ccaeeaebc5d123
Dariusz Hatala <hatala.dariusz@mailbox.org>
2019-05-02 11:50:16 +0200

    Add Pet Owner List controller and template Closes #16

---
+++ pet-clinic-web/src/main/java/hatala/dariusz/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package hatala.dariusz.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping({"/", "", "index", "index.html"})
+    public String listOfVets(){
+
+        return "owners/index";
+    }
+}
