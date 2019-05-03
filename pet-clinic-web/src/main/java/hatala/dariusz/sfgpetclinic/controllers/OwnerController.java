75b11df3bb6a11c3ce31246fa007922fa4332119
Dariusz Hatala <hatala.dariusz@mailbox.org>
2019-05-03 17:25:18 +0200

    Added list of owners on owners index page. Closes #19

--- pet-clinic-web/src/main/java/hatala/dariusz/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/hatala/dariusz/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package hatala.dariusz.sfgpetclinic.controllers;
 
+import hatala.dariusz.sfgpetclinic.repository.OwnerRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerRepository ownerRepository;
+
+    public OwnerController(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
     @RequestMapping({"/", "", "index", "index.html"})
-    public String listOfVets(){
+    public String listOfVets(Model model){
+
+        model.addAttribute("owners", ownerRepository.findAll() );
 
         return "owners/index";
     }
