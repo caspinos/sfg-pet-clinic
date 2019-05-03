843a0acc687dee95981aea10b2c3febfd35a707e
Dariusz Hatala <hatala.dariusz@mailbox.org>
2019-05-03 17:29:39 +0200

    Added list of vets on vets index page. Closes #18

--- pet-clinic-web/src/main/java/hatala/dariusz/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/hatala/dariusz/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package hatala.dariusz.sfgpetclinic.controllers;
 
+import hatala.dariusz.sfgpetclinic.repository.VetRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetRepository vetRepository;
+
+    public VetController(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
     @RequestMapping({"/", "", "index", "index.html"})
-    public String listOfVets(){
+    public String listOfVets(Model model){
+
+        model.addAttribute("vets", vetRepository.findAll());
 
         return "vets/index";
     }
