3e4b6d2d1533752f07de6e03e8437b10bf63b910
Dariusz Hatala <hatala.dariusz@mailbox.org>
2019-05-05 20:17:06 +0200

    Bootstrap PetTypes on startup. Closes #35

--- pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/repository/map/PetTypeRepositoryMap.java
+++ pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/repository/map/PetTypeRepositoryMap.java
@@ -1,8 +1,9 @@
 package hatala.dariusz.sfgpetclinic.repository.map;
 
 import hatala.dariusz.sfgpetclinic.model.PetType;
+import hatala.dariusz.sfgpetclinic.repository.PetTypeRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class PetTypeRepositoryMap extends MapRepositoryAbstract<PetType> {
+public class PetTypeRepositoryMap extends MapRepositoryAbstract<PetType> implements PetTypeRepository {
 }
--- pet-clinic-web/src/main/java/hatala/dariusz/sfgpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/hatala/dariusz/sfgpetclinic/bootstrap/DataInitializer.java
@@ -1,35 +1,59 @@
 package hatala.dariusz.sfgpetclinic.bootstrap;
 
-import hatala.dariusz.sfgpetclinic.model.Owner;
-import hatala.dariusz.sfgpetclinic.model.Vet;
+import hatala.dariusz.sfgpetclinic.model.*;
 import hatala.dariusz.sfgpetclinic.repository.OwnerRepository;
+import hatala.dariusz.sfgpetclinic.repository.PetRepository;
+import hatala.dariusz.sfgpetclinic.repository.PetTypeRepository;
 import hatala.dariusz.sfgpetclinic.repository.VetRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.HashSet;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
     private final OwnerRepository ownerRepository;
     private final VetRepository vetRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
 
-    @Autowired
-    public DataInitializer(OwnerRepository ownerRepository, VetRepository vetRepository) {
+    public DataInitializer(OwnerRepository ownerRepository, VetRepository vetRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
         this.ownerRepository = ownerRepository;
         this.vetRepository = vetRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        System.out.println("Loaded initial data");
+        System.out.println("Loading initial data");
 
-        ownerRepository.save( new Owner("John", "Smith") );
-        ownerRepository.save( new Owner("Julie", "Smith") );
+        // PetType init
+        PetType dog = new PetType("dog");
+        PetType cat = new PetType("cat");
+        petTypeRepository.save( dog );
+        petTypeRepository.save( cat );
 
-        vetRepository.save( new Vet("Adam", "Burner") );
-        vetRepository.save( new Vet("Mike", "Robinson") );
+        // Owner init
+        Owner john = new Owner("John", "Smith", new HashSet<>());
+        Owner julie = new Owner("Julie", "Smith", new HashSet<>());
+        ownerRepository.save( john );
+        ownerRepository.save( julie );
 
+        // Pet init
+        Pet roxy = new Pet(dog, john, LocalDate.of(1999, 2, 2));
+        Pet maisy = new Pet(cat, julie, LocalDate.of(2015, 1, 21));
+        petRepository.save( roxy );
+        petRepository.save( maisy );
+
+        // Vet init
+        Vet adam = new Vet("Adam", "Burner", new Speciality("Surgeon"));
+        Vet mike = new Vet("Mike", "Robinson", new Speciality("Cat Veterinary"));
+
+        vetRepository.save( adam );
+        vetRepository.save( mike );
     }
 }
