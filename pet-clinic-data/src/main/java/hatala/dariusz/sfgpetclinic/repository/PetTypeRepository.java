1b6f6a211e1872f115daf3dabc13cac43b33e156
Dariusz Hatala <hatala.dariusz@mailbox.org>
2019-05-05 19:57:01 +0200

    Create Pet Map Repository Closes #32

---
+++ pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/repository/PetTypeRepository.java
@@ -0,0 +1,6 @@
+package hatala.dariusz.sfgpetclinic.repository;
+
+import hatala.dariusz.sfgpetclinic.model.PetType;
+
+public interface PetTypeRepository extends BaseEntityCrudRepository<PetType> {
+}
---
+++ pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/repository/map/PetTypeRepositoryMap.java
@@ -0,0 +1,8 @@
+package hatala.dariusz.sfgpetclinic.repository.map;
+
+import hatala.dariusz.sfgpetclinic.model.PetType;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PetTypeRepositoryMap extends MapRepositoryAbstract<PetType> {
+}
