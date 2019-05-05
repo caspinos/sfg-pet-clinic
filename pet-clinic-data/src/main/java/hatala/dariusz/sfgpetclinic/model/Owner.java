cee277009103f83f1cebdcad9a940cbf2ce84314
Dariusz Hatala <hatala.dariusz@mailbox.org>
2019-05-05 19:50:48 +0200

    Add Contact Info Properties to Owner Closes #31

--- pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/model/Owner.java
@@ -3,7 +3,12 @@ package hatala.dariusz.sfgpetclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
+
     private Set<Pet> pets;
+
 
     public Owner() {
         super();
@@ -12,6 +17,38 @@ public class Owner extends Person {
     public Owner(String firstName, String lastName, Set<Pet> pets) {
         super(firstName, lastName);
         this.pets = pets;
+    }
+
+    public Owner(String firstName, String lastName, Set<Pet> pets, String address, String city, String telephone) {
+        super(firstName, lastName);
+        this.pets = pets;
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     public Set<Pet> getPets() {
