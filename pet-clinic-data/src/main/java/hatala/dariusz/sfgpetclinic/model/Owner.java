8c45b2b65047e61598f11c923fb0b59fce7038e3
Dariusz Hatala <hatala.dariusz@mailbox.org>
2019-05-05 19:43:54 +0200

    Create PetType, Pet and Visit Entities  Closes #29

--- pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/model/Owner.java
@@ -1,10 +1,24 @@
 package hatala.dariusz.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+    private Set<Pet> pets;
+
     public Owner() {
+        super();
     }
 
-    public Owner(String firstName, String lastName) {
+    public Owner(String firstName, String lastName, Set<Pet> pets) {
         super(firstName, lastName);
+        this.pets = pets;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
