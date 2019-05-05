b96105e9ba7081c2be2f2f60b0434fd8a2213e13
Dariusz Hatala <hatala.dariusz@mailbox.org>
2019-05-05 19:47:55 +0200

    Create Vet Specialty Entity and associate it to Vet Closes 30

--- pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/model/Vet.java
@@ -2,10 +2,21 @@ package hatala.dariusz.sfgpetclinic.model;
 
 public class Vet extends Person {
 
+    private Speciality speciality;
+
     public Vet() {
     }
 
-    public Vet(String firstName, String lastName) {
+    public Vet(String firstName, String lastName, Speciality speciality) {
         super(firstName, lastName);
+        this.speciality = speciality;
+    }
+
+    public Speciality getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Speciality speciality) {
+        this.speciality = speciality;
     }
 }
