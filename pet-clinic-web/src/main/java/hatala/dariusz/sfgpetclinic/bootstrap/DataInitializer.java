9d7cf427beafeada854c9cb247c0abe4718a3ee6
Dariusz Hatala <hatala.dariusz@mailbox.org>
2019-05-06 16:28:51 +0200

    Create Specialities, add to Vets on Startup Closes #33

--- pet-clinic-web/src/main/java/hatala/dariusz/sfgpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/hatala/dariusz/sfgpetclinic/bootstrap/DataInitializer.java
@@ -1,10 +1,7 @@
 package hatala.dariusz.sfgpetclinic.bootstrap;
 
 import hatala.dariusz.sfgpetclinic.model.*;
-import hatala.dariusz.sfgpetclinic.repository.OwnerRepository;
-import hatala.dariusz.sfgpetclinic.repository.PetRepository;
-import hatala.dariusz.sfgpetclinic.repository.PetTypeRepository;
-import hatala.dariusz.sfgpetclinic.repository.VetRepository;
+import hatala.dariusz.sfgpetclinic.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -18,19 +15,29 @@ public class DataInitializer implements CommandLineRunner {
     private final VetRepository vetRepository;
     private final PetRepository petRepository;
     private final PetTypeRepository petTypeRepository;
+    private final SpecialityRepository specialityRepository;
 
-    public DataInitializer(OwnerRepository ownerRepository, VetRepository vetRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
+    public DataInitializer(OwnerRepository ownerRepository, VetRepository vetRepository, PetRepository petRepository, PetTypeRepository petTypeRepository, SpecialityRepository specialityRepository) {
         this.ownerRepository = ownerRepository;
         this.vetRepository = vetRepository;
         this.petRepository = petRepository;
         this.petTypeRepository = petTypeRepository;
+        this.specialityRepository = specialityRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        System.out.println("Loading initial data");
+        if( petRepository.findAll().size() == 0) {
+            System.out.println("Creating initial data");
+            loadData();
+        } else {
+            System.out.println("Skipping initial data load");
+        }
 
+    }
+
+    private void loadData() {
         // PetType init
         PetType dog = new PetType("dog");
         PetType cat = new PetType("cat");
@@ -55,9 +62,17 @@ public class DataInitializer implements CommandLineRunner {
         petRepository.save( roxy );
         petRepository.save( maisey );
 
+        // Speciality init
+        Speciality surgery = new Speciality("Surgery");
+        Speciality radiology = new Speciality("Radiology");
+        Speciality dentistry= new Speciality("Dentistry");
+        specialityRepository.save( surgery );
+        specialityRepository.save( radiology );
+        specialityRepository.save( dentistry );
+
         // Vet init
-        Vet adam = new Vet("Adam", "Burner", new Speciality("Surgeon"));
-        Vet mike = new Vet("Mike", "Robinson", new Speciality("Cat Veterinary"));
+        Vet adam = new Vet("Adam", "Burner", surgery);
+        Vet mike = new Vet("Mike", "Robinson", dentistry);
 
         vetRepository.save( adam );
         vetRepository.save( mike );
