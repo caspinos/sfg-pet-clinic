9fcc3458e46de77e0409b9c6ea231e2c3bd07de1
Dariusz Hatala <hatala.dariusz@mailbox.org>
2019-05-08 16:35:05 +0200

    Implement Owners as JPA Entities Closes #39

--- pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/model/Pet.java
@@ -1,52 +1,31 @@
 package hatala.dariusz.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
-    public Pet() {}
-
-    public Pet(String name, PetType petType, Owner owner, LocalDate birthDate) {
-        this.name = name;
-        this.petType = petType;
-        this.owner = owner;
-        this.birthDate = birthDate;
-    }
-
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/model/PetType.java
@@ -1,19 +1,21 @@
 package hatala.dariusz.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pet_types")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class PetType extends BaseEntity{
+
+    @Column
     private String name;
 
-    public PetType() {}
-
-    public PetType(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
