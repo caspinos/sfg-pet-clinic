9d6877c793d81746cd021cd2eafd77149c4409d9
Dariusz Hatala <hatala.dariusz@mailbox.org>
2019-05-14 21:18:36 +0200

    Create Spring Data JPA Service for Vets Closes #44

--- pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/repository/VetRepository.java
+++ pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/repository/VetRepository.java
@@ -4,7 +4,10 @@ import hatala.dariusz.sfgpetclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VetRepository extends CrudRepository<Vet, Long> {
 
+    Optional<Vet> findByLastName(String lastName);
 }
