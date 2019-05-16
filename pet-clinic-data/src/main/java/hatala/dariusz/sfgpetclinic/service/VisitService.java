8f3104380b1a3008284a7e7b1a6770491f010ae4
Dariusz Hatala <hatala.dariusz@mailbox.org>
2019-05-16 18:19:57 +0200

    Create Visit Map Service.  Closes #49

---
+++ pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/service/VisitService.java
@@ -0,0 +1,6 @@
+package hatala.dariusz.sfgpetclinic.service;
+
+import hatala.dariusz.sfgpetclinic.model.Visit;
+
+public interface VisitService extends BaseEntityCrudService<Visit> {
+}
---
+++ pet-clinic-data/src/main/java/hatala/dariusz/sfgpetclinic/service/map/VisitServiceMap.java
@@ -0,0 +1,9 @@
+package hatala.dariusz.sfgpetclinic.service.map;
+
+import hatala.dariusz.sfgpetclinic.model.Visit;
+import hatala.dariusz.sfgpetclinic.service.VisitService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitServiceMap extends MapServiceAbstract<Visit> implements VisitService {
+}
